f49054a4ca40a78e6f43d5440989f540c9ef6df6
kimdaye77 <kimdaye77@naver.com>
2023-11-23 17:59:11 +0900

    [CHORE] 기본 경로 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
-import NotFound from "./routes/NotFound";
 
 interface IRouterProps {}
 
 function Router({}: IRouterProps) {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/crypto-tracker">
       <Routes>
-        <Route path="/:coinId" element={<Coin />} />
         <Route path="/" element={<Coins />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="/:coinId" element={<Coin />} />
       </Routes>
     </BrowserRouter>
   );
